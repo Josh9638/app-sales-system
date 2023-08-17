b119f7705acd4599e1dfc099b3ba997d6efc3204
Josh9638 <joshua.sailemab@ug.edu.ec>
2023-08-17 17:56:28 -0500

    Se crean componentes para el menu sidenav y los diferentes módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdministracionComponent } from './administracion/administracion.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'administracion', component: AdministracionComponent},
+  {path: 'inventario', component: InventarioComponent},
+  {path: 'ventas', component: VentasComponent},
+  {path: 'usuarios', component: UsuariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { AdministracionComponent } from './administracion/administracion.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BodyComponent,
+    SidenavComponent,
+    AdministracionComponent,
+    InventarioComponent,
+    VentasComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
